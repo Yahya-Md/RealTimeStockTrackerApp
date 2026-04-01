5ca7690b6859583f33ea0a4c442d2a03403ca180
Yahya Mahfoudi <yahya.mahfoudi@renault.com>
2026-04-01 19:39:02 +0100

    adding JsonLoader tests

--- RealTimeStockTrackerTests/Core/JsonLoaderTests.swift
+++ RealTimeStockTrackerTests/Core/JsonLoaderTests.swift
@@ -6,30 +6,58 @@
 //
 
 import XCTest
+import RealTimeStockTracker
 
 final class JsonLoaderTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    private class User: Decodable {
+        let id: Int
+        let name: String
+        
+        init(id: Int, name: String) {
+            self.id = id
+            self.name = name
         }
     }
+    
+    func testFetch_Success_onFoundedFileWithValidJson() async throws {
+        let sut = makeSUT()
+        let result = User(id: 1, name: "test")
+        let user: User = try await sut.fetch(for: "mock")
+        
+        XCTAssertEqual(user.id, result.id)
+        XCTAssertEqual(user.name, result.name)
+    }
+    
+    func testFetch_Error_onNotFoundedFile() async throws {
+        let sut = makeSUT()
+        await expect(sut: sut, fileName: "something", withError: .fileNotFound)
+    }
+    
+    func testFetch_Error_onFoundedFileWithInvalidJson() async {
+        let sut = makeSUT()
+        await expect(sut: sut, fileName: "invalid", withError: .failedToDecode)
+    }
+}
 
+extension JsonLoaderTests {
+    func makeSUT() -> JsonLoader {
+        return JsonLoader(bundle: Bundle(for: JsonLoaderTests.self))
+    }
+    
+    func expect(sut: JsonLoader,
+                fileName: String,
+                withError expectedError: JsonLoader.JsonLoaderError,
+                file: StaticString = #filePath,
+                line: UInt = #line
+    ) async {
+        do {
+            let _: User = try await sut.fetch(for: fileName)
+            XCTFail("Expected to get error \(expectedError)", file: file, line: line)
+        } catch let error as JsonLoader.JsonLoaderError {
+            XCTAssertEqual(error, expectedError, file: file, line: line)
+        } catch {
+            XCTFail("Wrong error type: \(error) instead of \(expectedError)", file: file, line: line)
+        }
+    }
 }
