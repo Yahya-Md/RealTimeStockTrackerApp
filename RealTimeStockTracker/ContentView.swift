28e66671bbd9cbba6c908f962aa178697572d665
Yahya Mahfoudi <yahya.mahfoudi@renault.com>
2026-04-01 18:38:50 +0100

    initial commit

---
+++ RealTimeStockTracker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RealTimeStockTracker
+//
+//  Created by Renault on 1/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RealTimeStockTracker/RealTimeStockTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeStockTrackerApp.swift
+//  RealTimeStockTracker
+//
+//  Created by Renault on 1/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeStockTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
