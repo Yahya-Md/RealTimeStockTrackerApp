8d2e9c07183b93b9d2daa55110650fbd46206e47
Yahya Mahfoudi <yahya.mahfoudi@renault.com>
2026-04-05 00:52:08 +0100

    Add Stocks List service

--- RealTimeStockTracker/Services/StocksListService.swift
+++ RealTimeStockTracker/Services/StocksListService.swift
@@ -2,3 +2,27 @@
 //  StocksListService.swift
 //  RealTimeStockTracker
 //
+
+public struct StockResponse: Decodable {
+    let symbol: String
+    let companyName: String
+    let stockDescription: String
+    let basePrice: Double
+}
+
+public protocol StocksListService {
+    typealias Stocks = [StockResponse]
+    func getStocks() async throws -> Stocks
+}
+
+public class StocksListServiceImpl: StocksListService {
+    private let loader: JsonLoadable
+    
+    public init(loader: JsonLoadable) {
+        self.loader = loader
+    }
+    
+    public nonisolated func getStocks() async throws -> Stocks {
+        return try await loader.fetch(for: "stocks")
+    }
+}
