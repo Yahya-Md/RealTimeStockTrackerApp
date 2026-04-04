fd60c6129e4eb417669e7630e71343d5cb804bb2
Yahya Mahfoudi <yahya.mahfoudi@renault.com>
2026-04-05 00:53:02 +0100

    add error test for the StocksListService

--- RealTimeStockTrackerTests/Services/StocksListServiceTests.swift
+++ RealTimeStockTrackerTests/Services/StocksListServiceTests.swift
@@ -6,30 +6,33 @@
 //
 
 import XCTest
+import RealTimeStockTracker
 
 final class StocksListServiceTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_getStocks_retrunError_onLoaderError() async {
+        let (sut, loader) = makeSUT()
+        let expectedError = NSError(domain: "", code: 0)
+        loader.expectedError = expectedError
+        do {
+            let _ = try await sut.getStocks()
+            XCTFail("Should throw error")
+        } catch(let error) {
+            XCTAssertEqual(error as NSError, expectedError)
         }
     }
+}
 
+extension StocksListServiceTests {
+    func makeSUT() -> (sut: StocksListServiceImpl,loader: JsonLoadableMock) {
+        let loader = JsonLoadableMock()
+        let sut = StocksListServiceImpl(loader: loader)
+        return (sut, loader)
+    }
+}
+
+class JsonLoadableMock: JsonLoadable {
+    var expectedError: Error?
+    func fetch<T>(for name: String) async throws -> T where T : Decodable {
+        throw expectedError ?? NSError()
+    }
 }
